d73a8aa92a670d2c268674b364cfc92d40c9b129
Leonardo Dino <leonardo@kunstdesign.com.br>
2014-11-26 13:12:18 -0200

    change dploy app enviroment strategy

---
+++ app/env.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ *	online test enviroment
+ *
+$__env = [ 
+	"name" => "mini-cms.test",
+	
+	"php_error" => false
+];
+
+/* 
+ *	production enviroment
+ *
+$__env = [ 
+	"name" => "mini-cms.production",
+	
+	"perfLogs"  => false,
+	"php_error" => false
+];
+/*
+ */
+ 
